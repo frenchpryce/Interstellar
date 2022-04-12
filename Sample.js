a1e1af188d11303047a279e094da6d39b4284a8a
frenchpryce <56567996+frenchpryce@users.noreply.github.com>
2022-04-13 06:37:49 +0800

    Create Sample.js

---
+++ Sample.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.google.com/specimen/Roboto?query=robo">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: flex;
+}
+
+.column {
+  flex: 50%;
+  padding: 10px;
+  height: 300px;
+}
+  
+</style>
+</head>
+<body>
+
+<div class="row" style="width: 40%; border: 2px solid black; border-radius: 10px;">
+  <div class="column" style="background-color:#fff; padding: 0px;">
+    <image style="width: 100%; height: 100%;" src="https://i.imgur.com/sKV54PO.jpeg" />
+  </div>
+  <div class="column" style="background-color:#fff; justifyContent: center; alignContent: center; padding: 50px;">
+    <h1 style="font-weight: bold; font-size: 20px;">Interstellar - A Documentary</h1>
+    <p>Sed Posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+    <button class="btn btn-success">LEARN MORE</button>
+  </div>
+</div>
+
+</body>
+</html>
